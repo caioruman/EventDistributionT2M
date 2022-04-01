e502d5bcbb2b85fd5d19dd6bba4a533dbe83c8d1
Caio J Ruman <caioruman@gmail.com>
2022-04-01 09:39:57 -0400

    adding the pr calculations

--- calc_events_array_pr.py
+++ calc_events_array_pr.py
@@ -54,17 +54,19 @@ def main():
       #if (m < 5 or m > 9):
       #  continue
 
-      if os.path.exists(f"{store}/t2m_{y}_{m:02d}.p"):
+      if os.path.exists(f"{store}/t2m_pr1_{y}_{m:02d}.p"):
         print("jump")
         continue      
       
       #/chinook/marinier/CONUS_2D/wrf2d_d01_CTRL_PREC_ACC_NC_200010-200012.nc
       sn = xr.open_dataset(f'{st}/{y}/wrf2d_d01_CTRL_T2_{y}{mi:02d}-{y}{mf:02d}.nc', engine='netcdf4')      
+      pr = xr.open_dataset(f'{st}/{y}/wrf2d_d01_CTRL_PREC_ACC_NC_{y}{mi:02d}-{y}{mf:02d}.nc', engine='netcdf4')      
 
       # Slicing the domain to make the computations faster
       #i1=721; j1=1167; i2=874; j2=1333
             
       sn = sn.T2
+      pr = pr.PREC_ACC_NC
       
       xlat = sn.XLAT
       xlon = sn.XLONG
@@ -82,16 +84,30 @@ def main():
       #print(wsn)
       
       sn = sn.sel(Time=slice(datai.strftime('%Y-%m-%d %H:%M'), dataf.strftime('%Y-%m-%d %H:%M')))      
-      
+      pr = pr.sel(Time=slice(datai.strftime('%Y-%m-%d %H:%M'), dataf.strftime('%Y-%m-%d %H:%M')))
+
+      sn_pr1 = sn.where(pr > 1, sn, np.nan)
+      sn_pr2 = sn.where(pr > 2, sn, np.nan)
+
       # Boolean array with the places where the temperature is between -2 and 2
-      count_bool_le = np.less_equal(sn.values, 271.15)
-      count_bool_ge = np.greater_equal(sn.values, 275.15)      
+      count_bool_le = np.less_equal(sn_pr1.values, 271.15)
+      count_bool_ge = np.greater_equal(sn_pr1.values, 275.15)      
       between = count_bool_ge + count_bool_le
       aux = np.invert(between)
       # Counting all the 1 values in the time dimension
       count = np.count_nonzero(aux.astype(int), axis=0)
            
-      pickle.dump( count, open( f"{store}/t2m_{y}_{m:02d}.p", "wb" ) )                  
+      pickle.dump( count, open( f"{store}/t2m_pr1_{y}_{m:02d}.p", "wb" ) )   
+
+      # Boolean array with the places where the temperature is between -2 and 2
+      count_bool_le = np.less_equal(sn_pr2.values, 271.15)
+      count_bool_ge = np.greater_equal(sn_pr2.values, 275.15)      
+      between = count_bool_ge + count_bool_le
+      aux = np.invert(between)
+      # Counting all the 1 values in the time dimension
+      count = np.count_nonzero(aux.astype(int), axis=0)
+           
+      pickle.dump( count, open( f"{store}/t2m_pr2_{y}_{m:02d}.p", "wb" ) )                
 
 if __name__ == '__main__':
   main()
